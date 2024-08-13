6de733e20e673c58497700414f9e602b5cdd4609
Biki Balami <bikibalami99@gmail.com>
2024-08-13 21:12:37 +0900

    Fix build issues: add Babel plugin and remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import BusinessList from "./Components/BusinessList/BusinessList";
-import SearchBar from "./Components/SearchBar/SearchBar";
 import HeroSection from "./Components/HeroSection/HeroSection";
 
 function App() {
