f42f729f81f7b9b9a1c6c05e5347e8982b3ff121
Biki Balami <bikibalami99@gmail.com>
2024-08-14 20:14:50 +0900

    change the shape of each card to square with updated animations

--- src/Components/Business/Business.tsx
+++ src/Components/Business/Business.tsx
@@ -10,26 +10,35 @@ interface BusinessProp {
 const Business = ({ aBusiness }: BusinessProp): JSX.Element => {
   return (
     <li className={styles.eachBusiness}>
-      <img
-        className={styles.businessImg}
-        src={aBusiness.imageSrc}
-        alt="Current Business"
-      />
-      <p className={styles.category}>{aBusiness.category}</p>
-      <article className={styles.cardContent}>
-        <h2 className={styles.cardTitle}>{aBusiness.name}</h2>
-        <section className={styles.cardInformation}>
-          <section className={styles.review}>
-            <RatingStars rating={aBusiness.rating} />
-            <p>{"(" + aBusiness.reviewCount + " reviews)"}</p>
+      {/* Link to the website */}
+      <a className={styles.link} href="#">
+        {/* Background Image */}
+        <img
+          className={styles.businessImg}
+          src={aBusiness.imageSrc}
+          alt="Current Business"
+        />
+        {/* Category of the food*/}
+        <p className={styles.category}>{aBusiness.category}</p>
+
+        {/* Main content */}
+        <article className={styles.cardContent}>
+          <h2 className={styles.cardTitle}>{aBusiness.name}</h2>
+
+          <section className={styles.cardInformation}>
+            <section className={styles.review}>
+              <RatingStars rating={aBusiness.rating} />
+              <p>{"(" + aBusiness.reviewCount + " reviews)"}</p>
+            </section>
+
+            <address className={styles.address}>
+              <p>{aBusiness.address}</p>
+              <p>{aBusiness.city + ", " + aBusiness.state}</p>
+              <p>{"Zipcode:" + aBusiness.zipCode}</p>
+            </address>
           </section>
-          <address className={styles.address}>
-            <p>{aBusiness.address}</p>
-            <p>{aBusiness.city + ", " + aBusiness.state} </p>
-            <p>{"Zipcode:" + aBusiness.zipCode}</p>
-          </address>
-        </section>
-      </article>
+        </article>
+      </a>
     </li>
   );
 };
